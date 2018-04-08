b12395c80e778a328cf5770d7b8e5c22828572d9
alwanputra2712 <37875696+alwanputra2712@users.noreply.github.com>
2018-04-08 21:27:16 +0700

    Update Poll.py

--- LINETCR/Api/Poll.py
+++ LINETCR/Api/Poll.py
@@ -18,8 +18,8 @@ class Poll:
   host = "gd2.line.naver.jp";
   port = 443;
 
-  UA = "Line/7.14.0"
-  LA = "WIN10\t8.0.0\tHelloWorld\t11.2.5"
+  UA = "Line/8.3.2"
+  LA = "DESKTOPWIN\t8.3.2\tWib-Oswib\t10.0.0"
 
   rev = 0
 
