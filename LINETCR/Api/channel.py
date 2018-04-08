c684234699ed5e71a32de5751e96be67c447eef4
alwanputra2712 <37875696+alwanputra2712@users.noreply.github.com>
2018-04-08 21:29:42 +0700

    Update channel.py

--- LINETCR/Api/channel.py
+++ LINETCR/Api/channel.py
@@ -18,8 +18,8 @@ class Channel:
     http_query_path = "/S4"
     channel_query_path = "/CH4"
 
-    UA = "Line/7.14.0"
-    LA = "WIN10\t8.0.0\tHelloWorld\t11.2.5WIN10"
+    UA = "Line/8.3.2"
+    LA = "DESKTOPWIN\t8.3.2\tWib-Oswib\t10.0.0"
 
     authToken = None
     mid = None
